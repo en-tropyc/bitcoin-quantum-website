10d6313d619804eea9e594bdfd6b793d2dc4277b
hunterhsiao <hsiaohunter@gmail.com>
2025-10-08 15:39:25 -0400

    updating introduction tab

--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -147,7 +147,7 @@ export default function Introduction() {
                 </p>
                 <div className="flex flex-col sm:flex-row gap-4 justify-center">
                   <Link
-                    href="/resources"
+                    href="/development"
                     className="bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-700 transition-colors font-dm-mono"
                   >
                     View Development
